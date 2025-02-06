8ef778d57457adae26272553ec91996883b84454
Your Name <you@example.com>
2025-02-06 11:47:52 -0500

    Init comit

---
+++ Python_scope.py
@@ -0,0 +1,13 @@
+import time
+
+ma_variable_globale = "variable globale"
+
+def ma_fonction(ma_variable_argument):
+    ma_variable_locale = "variable locale"
+    for i in range(3):
+        print(ma_variable_globale)
+        print(ma_variable_argument)
+        print(ma_variable_locale)
+        time.sleep(1)
+
+ma_fonction("variable argument")
